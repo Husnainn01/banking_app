6c81f8464ac2cccdb3bdd9d59ec87e4e6b5607dd
HUS NAIN <husnain@ss-japan.com>
2024-06-26 10:40:04 +0900

    added the AnimatedCounter Button in the Components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
+    const loggedIn = {
+        FirstName: "Husnain"
+    };
   return (
-    <div>Home</div>
+    <section className='home'>
+       <div className='home-content'>
+        <header className='home-header'>
+            <HeaderBox 
+                type='greeting'
+                title='Welcome'
+                user={loggedIn?.FirstName || 'Guest'}
+                subtext= 'Access and Manage your Account Transactions efficiently.' 
+            />
+        <TotalBalanceBox 
+         accounts={[]}
+         totalBanks={1}
+         totalCurrentBalance={1250.35}
+        />
+        </header>
+       </div>
+    </section>
   )
 }
 
